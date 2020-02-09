58a9bf755f57c616f03fe479932bca0dd2a9355c
RicardoTurco <ricturco@gmail.com>
2020-02-09 00:48:26 -0300

    Adjust model Planilha

--- importing/models.py
+++ importing/models.py
@@ -10,3 +10,9 @@ class Planilha(models.Model):
     nome_pessoa = models.CharField(max_length=150)
     tel_pessoa = models.CharField(max_length=50, null=True, blank=True)
     sexo = models.CharField(max_length=15, null=True, blank=True)
+    
+    class Meta:
+        verbose_name_plural = 'Planilha'
+        
+    def __str__(self):
+        return "Data: %s - Pessoa: %s - Demanda: %s" % (self.data, self.nome_pessoa, self.demanda)
