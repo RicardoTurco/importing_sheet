8948b6ff4c74d33bfb31c2bac662d743ef6a9204
RicardoTurco <ricturco@gmail.com>
2020-02-09 00:08:41 -0300

    Create model Planilha

--- importing/models.py
+++ importing/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Planilha(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    demanda = models.CharField(max_length=150)
+    categoria = models.CharField(max_length=30)
+    data = models.DateField()
+    nome_pessoa = models.CharField(max_length=150)
+    tel_pessoa = models.CharField(max_length=50, null=True, blank=True)
+    sexo = models.CharField(15, null=True, blank=True)
